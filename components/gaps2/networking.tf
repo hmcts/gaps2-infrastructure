76cb7871aee687e099731b0375e1cd0fadf1a1e5
Marty Fox <martin.fox1@justice.gov.uk>
2025-05-06 10:26:41 +0100

    update networking to use vnet provider

--- components/gaps2/networking.tf
+++ components/gaps2/networking.tf
@@ -1,8 +1,9 @@
 resource "azurerm_subnet" "gaps2" {
+  provider = azurerm.vnet
 
   name                 = "gaps2-subnet"
-  resource_group_name  = var.virtual_network.resource_group_name
-  virtual_network_name = var.virtual_network.virtual_network_name
+  resource_group_name  = data.virtual_network.jumpbox_vnet.resource_group_name
+  virtual_network_name = data.virtual_network.jumpbox_vnet.name
   address_prefixes     = [var.gaps2_subnet_range]
   service_endpoints    = ["storage"]
 
