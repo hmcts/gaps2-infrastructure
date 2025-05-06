502b2c3adf15ab8fb9f497ec0d3b136a9f078f7f
Marty Fox <martin.fox1@justice.gov.uk>
2025-05-06 10:59:15 +0100

    create subnets first then privatelink

--- components/gaps2/storageaccount.tf
+++ components/gaps2/storageaccount.tf
@@ -12,7 +12,7 @@ module "storage_account" {
   enable_hns  = true
   enable_sftp = true
 
-  private_endpoint_subnet_id = azurerm_subnet.gaps2.id
+  # private_endpoint_subnet_id = azurerm_subnet.gaps2.id
 
   common_tags = module.tags.common_tags
 }
