bdd21d674cbba5939b86a9ea891557dc44e10b8c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-16 15:06:29 +0100

    DTSPO-34656 - Update keyvault branch. (#11)
    * DTSPO-34656: update key vault module branch
    
    * update azurerm

--- components/gaps2/keyvault.tf
+++ components/gaps2/keyvault.tf
@@ -1,5 +1,5 @@
 module "key_vault" {
-  source = "github.com/hmcts/cnp-module-key-vault?ref=master"
+  source = "github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
 
   name                = "${var.product}-kv-${var.env}"
   resource_group_name = azurerm_resource_group.rg.name
